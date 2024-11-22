f3243bd33ba63b02ce11ec11606e9a4ee64e9fb5
Yunhich <berezovskii05@mail.ru>
2024-11-22 17:28:12 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+using namespace std;
+
+// Переменные
+pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int ready = 0;
+
+// Функция-поставщик
+void* producer(void* arg) {
+    while (true) {
+        pthread_mutex_lock(&lock);
+
+        if (ready == 1) {
+            pthread_mutex_unlock(&lock);
+            continue;
+        }
+
+        ready = 1;
+        cout << "provided\n";
+        pthread_cond_signal(&cond1);
+        pthread_mutex_unlock(&lock);
+    }
+    return 0;
+}
+
+// Функция-потребитель
+void* consumer(void* arg) {
+    while (true) {
+        pthread_mutex_lock(&lock);
+
+        while (ready == 0) {
+            pthread_cond_wait(&cond1, &lock);
+            cout << "awoke\n";
+        }
+
+        ready = 0;
+        cout << "consumed\n";
+        pthread_mutex_unlock(&lock);
+    }
+    return 0;
+}
+
+int main() {
+    pthread_t producerThread, consumerThread;
+
+    pthread_create(&producerThread, nullptr, producer, nullptr);
+    pthread_create(&consumerThread, nullptr, consumer, nullptr);
+
+    pthread_join(producerThread, nullptr);
+    pthread_join(consumerThread, nullptr);
+
+    return 0;
+}
